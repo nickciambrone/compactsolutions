8b7c30dc45d122d688554c1319c0a7cacd1e2917
Nick Ciambrone <nickciambrone@Nicks-MBP.fios-router.home>
2021-12-30 11:33:23 -0500

    not the same

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -5,11 +5,20 @@ import "./home.styles.scss";
 const Home = () => {
   return (
     <div className="home">
-   
       <div className="home-content-container">
-      <div className='intro'> Welcome to the official homepage of Compact Solutions</div>
-        <div className = 'project-area'>
-        <h5 style={{color:'#747474', marginBottom:'30px'}}>Our Recent Projects:</h5>
+        <div className="intro">
+          {" "}
+          <h4 style={{ color: "#666" }}>What do we do at Compact Solutions?</h4>
+          <div className = 'blue-line' style={{height:'2px', width:'75%', backgroundColor:'#0099ff', marginTop:'20px', marginBottom:'20px'}}></div>
+          <p> At Compact Solutions, we are dedicated to one goal: <b>provide businesses with the highest quality web and mobile applications.</b> </p>
+          <p> Which is why we make sure that our design process is consistent with the top development teams in the technology industry. We don't just build your app, we solve your problem. After the development takes place, we work with our clients to ensure that the application solves whatever problem they were aiming to fix, and also provides a smooth user experience.   </p>
+          <p>After the build, we don't wave our goodbyes. Our dedicated team is here to answer all questions and ensure 100% satisfaction. </p>
+
+        </div>
+        <div className="project-area">
+          <h5 style={{ color: "#747474", marginBottom: "20px" }}>
+            Our Recent Projects:
+          </h5>
           <CarouselContainer />
         </div>
       </div>
