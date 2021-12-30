d4f0bab4309c13c12c123dd71e0b96b13bb2947c
Nick Ciambrone <nickciambrone@Nicks-MBP.fios-router.home>
2021-12-30 13:17:58 -0500

    not the same

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from "react";
 
-import './contact.styles.scss'
+import "./contact.styles.scss";
+const FORM_ENDPOINT = "https://public.herotofu.com/v1/959cd980-6999-11ec-b2d5-af79f33d926c";
 
-const Contact = () =>{
-    return(
-        <div className = 'contact'> contact</div>
-    )
-}
+const Contact = () => {
+  return (
+    <div className="contact">
+
+    <h3 style={{color:'#666', marginTop:'12px'}}>Contact Form</h3>
+    <div className = 'blue-line' style={{height:'2px', width:'30%', marginLeft:'35%', backgroundColor:'#0099ff', marginTop:'20px', marginBottom:'20px'}}></div>
+
+      <form
+        action="https://public.herotofu.com/v1/959cd980-6999-11ec-b2d5-af79f33d926c"
+        method="post"
+      >
+        <div>
+          <input style={{padding:'14px 20px'}} name="Name" id="name" type="text" placeholder='Name'  required />
+        </div>
+        <div>
+          <input style={{padding:'14px 20px'}} name="Email" id="email" type="email" placeholder='Email' required />
+        </div>
+        <div style={{marginBottom:'5px'}}>
+        <input style={{padding:'14px 20px 60px 20px', height:'90px'    }} name="Message" id="message" type="text" placeholder='Message' required />
+      </div>
+      <div style={{marginBottom:'25px'}}>
+      <input type="submit" value="Submit" style={{width:'100px',padding:'10px 20px', backgroundColor:'#009aff', color:'white'}} />
+        </div>
+      </form>
+    </div>
+  );
+};
 export default Contact;
--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -6,13 +6,13 @@ const Home = () => {
   return (
     <div className="home">
       <div className="home-content-container">
-        <div className="intro">
+        <div className="intro" style={{marginTop:'16px'}}>
           {" "}
-          <h4 style={{ color: "#666" }}>What do we do at Compact Solutions?</h4>
-          <div className = 'blue-line' style={{height:'2px', width:'75%', backgroundColor:'#0099ff', marginTop:'20px', marginBottom:'20px'}}></div>
-          <p> At Compact Solutions, we are dedicated to one goal: <b>provide businesses with the highest quality web and mobile applications.</b> </p>
-          <p> Which is why we make sure that our design process is consistent with the top development teams in the technology industry. We don't just build your app, we solve your problem. After the development takes place, we work with our clients to ensure that the application solves whatever problem they were aiming to fix, and also provides a smooth user experience.   </p>
-          <p>After the build, we don't wave our goodbyes. Our dedicated team is here to answer all questions and ensure 100% satisfaction. </p>
+          <h4 style={{ color: "#666", textAlign:'center' }}>What do we do at Compact Solutions?</h4>
+          <div className = 'blue-line' style={{height:'2px', width:'90%', marginLeft:'5%', backgroundColor:'#0099ff', marginTop:'20px', marginBottom:'20px'}}></div>
+          <p style = {{color:'#666', lineHeight:'1.9'}}> At Compact Solutions, we are dedicated to one goal: <b>provide businesses with the highest quality web and mobile applications.</b> </p>
+          <p style = {{color:'#666', lineHeight:'1.9'}}> Which is why we make sure that our design process is consistent with the top development teams in the technology industry. We don't just build your app, we solve your problem. After the development takes place, we work with our clients to ensure that the application solves whatever problem they were aiming to fix, and also provides a smooth user experience.   </p>
+          <p style = {{color:'#666', lineHeight:'1.9'}}>After the build, we don't wave our goodbyes. Our dedicated team is here to answer all questions and ensure 100% satisfaction. </p>
 
         </div>
         <div className="project-area">
